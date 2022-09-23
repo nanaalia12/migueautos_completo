eb2b37548a380000a90d7a74807d01b398e35d8a
benyalf <joselinalfaro71@gmail.com>
2022-09-23 14:18:32 -0500

    Merge branch 'master' into joselin

--- facturacion/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from facturacion.models import *
-
-class FacturaForm(forms.ModelForm):
-    class Meta:
-        model = Generar
-        fields = ['usuario','vehiculo']
